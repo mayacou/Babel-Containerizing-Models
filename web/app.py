204d5f1e2a8d868e7662b6aa699078bbbe898bbc
paulvinueza30 <paulvinueza30@gmail.com>
2025-04-03 23:47:38 -0400

    fixed app.py to send back data as JSON for api

--- web/app.py
+++ web/app.py
@@ -1,71 +1,24 @@
-<<<<<<< Updated upstream
-def route_to_model(text, target_lang):
-
-    if target_lang == "bg":  
-        from useModels.use_helsinkiBg import translate  
-        return translate(text, "en", target_lang)
-
-    elif target_lang == "cs":
-        from useModels.use_helsinkiCs import translate  
-        return translate(text, "en", target_lang)
-
-    elif target_lang == "da":
-        from useModels.use_helsinkiDa import translate  
-        return translate(text, "en", target_lang)
-
-    elif target_lang == "de":
-        from useModels.use_helsinkiDe import translate  
-        return translate(text, "en", target_lang)
-
-=======
 import sys
 import json
 
 def translate_for_single_language(text, source_lang, target_lang):
     if target_lang in ["fr", "de"]:
         from useModels.use_example import translate
-        return {"target_lang": target_lang, "translated_text": translate(text=text, source_lang="eng_Latn", target_lang=f"{target_lang}_Latn")}
->>>>>>> Stashed changes
+        return {"target_lang": target_lang, "translated_text": translate(text, "eng_Latn", source_lang, f"{target_lang}_Latn")}
     elif target_lang in ["tr"]:
         from useModels.use_example import translate
-        return {"target_lang": target_lang, "translated_text": translate(text=text, source_lang=source_lang, target_lang=target_lang)}
+        return {"target_lang": target_lang, "translated_text": translate(text = text,  source_lang=source_lang, target_lang=target_lang)}
     elif target_lang in ["hu"]:
         from useModels.use_helsinki_hu import translate
-<<<<<<< Updated upstream
-        return translate(text)
-    
-    elif target_lang in ["sv"]:
-        from useModels.use_helsinki_en_sv import translate
-        return translate(text)
-    
-    elif target_lang in ["et"]:
-        from useModels.use_helsinki_en_et import translate
-        return translate(text)
-    
-    elif target_lang in ["el"]:
-        from useModels.use_helsinki_en_el import translate
-        return translate(text)
-    
-    elif target_lang in ["fr"]:
-        from useModels.use_helsinki_en_fr import translate
-        return translate(text)
-    
-    elif target_lang in ["fi"]:
-        from useModels.use_helsinki_en_fi import translate
-        return translate(text)
-    
-=======
-        return {"target_lang": target_lang, "translated_text": translate(text=text, source_lang=source_lang, target_lang=target_lang)}
->>>>>>> Stashed changes
+        return {"target_lang": target_lang, "translated_text": translate(text)}
     elif target_lang in ["lt"]:
         from useModels.use_helsinki_lt import translate
-        return {"target_lang": target_lang, "translated_text": translate(text=text, source_lang=source_lang, target_lang=target_lang)}
+        return {"target_lang": target_lang, "translated_text": translate(text)}
     elif target_lang in ["sk"]:
         from useModels.use_helsinki_sk import translate
-        return {"target_lang": target_lang, "translated_text": translate(text=text, source_lang=source_lang, target_lang=target_lang)}
+        return {"target_lang": target_lang, "translated_text": translate(text)}
     else:
         return {"target_lang": target_lang, "translated_text": "No model available for this language"}
-
 
 def route_to_model(text, source_lang, target_langs):
     translated_texts = []
@@ -75,7 +28,7 @@ def route_to_model(text, source_lang, target_langs):
     return translated_texts
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:  # Make sure there are enough arguments
+    if len(sys.argv) < 4:
         print("Usage: python model_router.py <source_text> <source_lang> <target_lang1> <target_lang2> ...")
         sys.exit(1)
 
@@ -85,5 +38,4 @@ if __name__ == "__main__":
 
     translated_texts = route_to_model(source_text, source_lang, target_langs)
 
-    # Print the result as a JSON array
     print(json.dumps(translated_texts))
